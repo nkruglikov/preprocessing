87ec190487e52afd31e38ba9aa2c0680a628dc96
Nikolai Kruglikov <kkrugl@yandex.ru>
2016-04-23 17:55:31 +0300

    Add part-of-speech tags

--- tex2json.py
+++ tex2json.py
@@ -8,11 +8,11 @@ import pickle
 import json
 
 # Define regexps
-re_title = re.compile(r'\\title.*?\{(.*?)\}', re.DOTALL|re.L|re.M)
-re_author = re.compile(r'\\author.*?\{(.*?)\}', re.DOTALL|re.L|re.M)
+re_title = re.compile(r'\\title.*?\{(.*?)\}', flags=re.DOTALL|re.L|re.M)
+re_author = re.compile(r'\\author.*?\{(.*?)\}', flags=re.DOTALL|re.L|re.M)
 re_text = re.compile(r'\\maketitle(.*?)'
     r'(?:\\begin\{thebibliography\}.*\\end\{thebibliography\}.*)?'
-    r'\\end\{document\}', re.DOTALL|re.L|re.M)
+    r'\\end\{document\}', flags=re.DOTALL|re.L|re.M)
 #re_norm_spaces = re.compile(r'(\s)+', re.DOTALL|re.L|re.M)
 #re_norm_mystem = re.compile(r'\?+\}', re.DOTALL|re.L|re.M)
 #re_jsonize = re.compile(r'(\w*)\{(.*?)\}', re.DOTALL|re.L|re.M)
@@ -20,24 +20,27 @@ re_text = re.compile(r'\\maketitle(.*?)'
 
 def detex(string):
     buf = bytes(string, encoding='utf8')
-    return subprocess.check_output(['detex', '-l'], input=buf).decode('utf8')
+    return subprocess.check_output(['detex', '-l', '-e',
+        '\'array,eqnarray,equation,figure,mathmatica,'
+        'picture,table,verbatim,align,multline\''], input=buf).decode('utf8')
 
 def normalize(string):
-    return re.sub(r'(\s)+', r' ', string, re.M|re.L|re.DOTALL)
+    return re.sub(r'(\s)+', r' ', string, flags=re.M|re.L|re.DOTALL)
 
 def mystem(string):
     buf = bytes(string, encoding='utf8')
-    analysis = subprocess.check_output(['./mystem', '-c', '-d'],
+    analysis = subprocess.check_output(['./mystem', '-cdi'],
             input=buf).decode('utf8')
-    return re.sub(r'\?+\}', r'}', analysis, re.M|re.L|re.DOTALL)
+    return re.sub(r'[?]+([}=])', r'\1', analysis, flags=re.M|re.L|re.DOTALL)
 
 def jsonize(word):
     result = {}
-    search = re.search(r'(\w*)\{(.*?)\}', word)
+    search = re.search(r'(\w*)\{(.*?)(?:=([A-Z]+).*?)?\}', word)
     if search is not None:
-        raw, lex = search.groups()
+        raw, lex, pos = search.groups()
         result['raw'] = raw
         result['lex'] = lex
+        result['pos'] = pos
     else:
         result['raw'] = word
     return result
@@ -69,7 +72,7 @@ def process_tex(tex_filename):
         paragraph = []
         for s in p:
             sentence = [jsonize(w) for w in
-                    re.split(r'(\w*\{.*?\})', s, re.L|re.M|re.DOTALL)]
+                    re.split(r'(\w*\{.*?\})', s)]
             paragraph.append(sentence)
         document['text'].append(paragraph)
 
